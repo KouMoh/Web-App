028c2bcd21ef7f1ff4a2db2b547e93973bc10099
KouMoh <126945657+KouMoh@users.noreply.github.com>
2025-01-12 23:22:02 +0530

    Add files via upload

---
+++ project/edit.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "website");
+
+$conn = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+if ($conn->connect_error) {
+    die("Connection failure: " . $conn->connect_error);
+}
+
+$name = $_POST['update-name'];
+$id = $_POST['id'];
+
+$stmt = $conn->prepare("UPDATE `login` SET `name`=? WHERE `id`=?");
+$stmt->bind_param("si", $name, $id);
+
+if ($stmt->execute()) {
+    $_SESSION['name'] = $name; 
+    echo 'success';
+} else {
+    echo 'Error updating record: ' . $conn->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
